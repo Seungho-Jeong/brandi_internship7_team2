bd451ab1a973c29c92dd905b614aa1e701ccfe04
lemongucci <developerdawnnn@gmail.com>
2020-11-24 02:54:33 +0900

    [Navbar > Sidebar] - 메뉴 리스트 항목의 하위 리스트를 클릭 여부에 따라 화면에 나타났다가 사라지게끔 rendering 조절 - 하위 리스트를 클릭하게 될 경우, 해당 항목의 페이지가 현재 빈 화면에 나타나도록 sidebar의 key값 및 value 값 조절할 예정 - v-for를 sidebar의 각 요소 그리고 그 안의 배열 길이에 따른 정수를 활용하여 메뉴 리스트 구성

---
+++ front_end/src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="header">
+    <div>
+      <img :src="logo">
+      staging(staging)
+    </div>
+    <div>
+      {{ userId }}
+    </div>
+  </div>
+
+  <div class="sidebar">
+    <ul>
+      <li v-for="item in sidebar">
+        {{ item.title }}
+        <!-- <ul>
+              <li v-for="item in sidebar">{{item.list}}</li>
+          </ul> -->
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+import './NavBar.scss';
+export default {
+  name: 'NavBar',
+  setup() {
+    const logo = ref('src/assets/logo_2.png');
+
+    const userId = ref('hahaha');
+    const sidebar = ref([
+      { title: '주문관리' },
+      { title: '상품관리' },
+      { title: '회원 관리' },
+      { title: '홈' },
+      { title: '' }
+    ]);
+  }
+};
+</script>
