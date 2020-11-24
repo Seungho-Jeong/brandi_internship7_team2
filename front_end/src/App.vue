68d68c8d98321389458f71326818b03eeb45cdd8
lemongucci <developerdawnnn@gmail.com>
2020-11-25 03:42:16 +0900

    [Login] - Routing 및 Validation 기능 작업 중

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,66 +1,40 @@
 <template>
-  <div class="container">
-    <form action="/action_page.php">
-      <label for="usrname">Username</label>
-      <input
-        :class="{invalid: }"
-        type="text"
-        id="usrname"
-        name="usrname"
-        required
-        placeholder="셀러 아이디"
-      >
-      <p :class="{}">
-아이디를 입력해주세요.
-</p>
-
-      <label for="psw">Password</label>
-      <input
-        :class="{}"
-        type="password"
-        id="psw"
-        name="psw"
-        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-        title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
-        required
-        placeholder="셀러 비밀번호"
-      >
-      <p :class="{}">
-비밀번호를 입력해주세요.
-</p>
-
-      <input type="submit"
-value="로그인"
-:onsubmit="submit"
-/>
-    </form>
-  </div>
-
-  <div id="message">
-    <h3>Password must contain the following:</h3>
-    <p
-id="letter" class="invalid">A <b>lowercase</b> letter</p>
-    <p
-id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
-    <p id="number"
-class="invalid"
->
-A <b>number</b>
-</p>
-    <p
-id="length" class="invalid">Minimum <b>8 characters</b></p>
+  <div id="app">
+    <div id="nav">
+      <router-link :to="{ name: 'EventList' }">
+Events
+</router-link> |
+      <router-link :to="{ name: 'About' }">
+About
+</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script lang="js">
-
-export default (
-methods : {
-
-  submit(){
-    fetch:
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-)
-</script>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+h4 {
+  font-size: 20px;
+}
+</style>
