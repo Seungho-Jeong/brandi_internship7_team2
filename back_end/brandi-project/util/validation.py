0dd66387b4ea08c54fedfd245efb0bb15fedc515
Daniel Jeong <sh007.jeong@gmail.com>
2020-12-08 09:51:23 +0900

    상품 신규등록 키워드 밸리데이터 적용

--- back_end/brandi-project/util/validation.py
+++ back_end/brandi-project/util/validation.py
@@ -61,25 +61,25 @@ class KeywordValidation:
     # 상품 신규 등록
     def create_new_product(self, data):
         lst = [
-            'seller_id'
-            'product_subcategory_id'
-            'product_name'
-            'price'
-            'manufacturing_country_id'
-            'manufacturing_date'
-            'manufacturing_company'
-            'short_introduction'
-            'product_detail_information'
-            'discount_ratio'
-            'discount_start_date'
-            'discount_end_date'
-            'min_sale_quantity'
-            'max_sale_quantity'
-            'registration_status_id'
-            'sale_status_id'
-            'display_status_id'
-            'colors'
-            'sizes'
+            'seller_id',
+            'product_subcategory_id',
+            'product_name',
+            'price',
+            'manufacturing_country_id',
+            'manufacturing_date',
+            'manufacturing_company',
+            'short_introduction',
+            'product_detail_information',
+            'discount_ratio',
+            'discount_start_date',
+            'discount_end_date',
+            'min_sale_quantity',
+            'max_sale_quantity',
+            'registration_status_id',
+            'sale_status_id',
+            'display_status_id',
+            'colors',
+            'sizes',
             'inventories'
         ]
         validation(data, lst, '상품 신규 등록')
